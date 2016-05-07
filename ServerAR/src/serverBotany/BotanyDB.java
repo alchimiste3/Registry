190ef6fb5f5840e275206dc80a1e6325ec2f2b53
Clément SIBUT <sc407514@etu.unice.fr>
2016-05-07 16:22:37 +0200

    Ajout client/serveur botanique

---
+++ ServerAR/src/serverBotany/BotanyDB.java
@@ -0,0 +1,26 @@
+package serverBotany;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+
+public class BotanyDB  extends UnicastRemoteObject implements BotanyDBInterface {
+        HashMap<String,Plant> plantes;
+
+    public BotanyDB() throws RemoteException {
+        super();
+        this.plantes = new HashMap<>();
+    }
+        
+
+    @Override
+    public Plant getPlant(String nom) throws RemoteException {
+        return plantes.get(nom);
+    }
+
+    @Override
+    public void addPlant(Plant p) throws RemoteException {
+        plantes.put(p.getNom(), p);
+    }
+    
+}
